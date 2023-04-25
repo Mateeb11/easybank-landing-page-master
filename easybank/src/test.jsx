3dadcd47d2baa00af85182e0fcb8aa8da20f50fc
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2023-04-25 15:28:19 +0300

    config sass and test it

---
+++ easybank/src/test.jsx
@@ -0,0 +1,11 @@
+import classes from "./test.module.scss";
+
+export default function Test() {
+  return (
+    <>
+      <div className={classes.div}>
+        hello<h1>hi</h1>
+      </div>
+    </>
+  );
+}
