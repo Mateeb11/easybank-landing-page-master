8591625d2d5318b6185164ed6dee03058754d4ac
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2023-05-01 18:45:17 +0300

    styling the header
    adding the hamburger icon and close icon. open the nav when the hamburger icon is clicked. add animation when the nav bar is opened

--- easybank/src/Component/Header/Header.jsx
+++ easybank/src/Component/Header/Header.jsx
@@ -1,33 +1,55 @@
+import { useState } from "react";
+
 import classes from "./Header.module.scss";
 
 import Button from "../UI/Button";
 
 import logo from "../../assets/logo.svg";
+import hamburger from "../../assets/icon-hamburger.svg";
+import close from "../../assets/icon-close.svg";
 
 export default function Header() {
+  const [openHamburger, setOpenHamburger] = useState(false);
+
+  const openHamburgerHandler = () => {
+    setOpenHamburger(!openHamburger);
+  };
   return (
-    <header className={classes.container}>
-      <img src={logo} alt="easy bank logo" />
-      <nav aria-label="primary">
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-            <a href="#">Blog</a>
-          </li>
-          <li>
-            <a href="#">Careers</a>
-          </li>
-        </ul>
-      </nav>
-      <Button></Button>
-    </header>
+    <>
+      <header className={classes.container}>
+        <img src={logo} alt="easy bank logo" />
+        <nav
+          aria-label="primary"
+          className={`${openHamburger && classes.open} ${classes.primaryNav}`}
+        >
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+          </ul>
+        </nav>
+        <Button className={`${classes.button}`}></Button>
+        <img
+          src={openHamburger ? close : hamburger}
+          alt={openHamburger ? "Close icon" : "Hamburger icon"}
+          onClick={openHamburgerHandler}
+        />
+      </header>
+      <div
+        className={` ${openHamburger && classes.open} ${classes.navBackground}`}
+      ></div>
+    </>
   );
 }
--- easybank/src/Component/UI/Button.jsx
+++ easybank/src/Component/UI/Button.jsx
@@ -1,5 +1,7 @@
 import classes from "./Button.module.scss";
 
-export default function Button() {
-  return <button className={classes.button}>Request Invite</button>;
+export default function Button({ className }) {
+  return (
+    <button className={`${classes.button} ${className}`}>Request Invite</button>
+  );
 }
