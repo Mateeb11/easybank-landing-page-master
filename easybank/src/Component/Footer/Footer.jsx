1e93e9ab4d18f2c89f7b053b82681a8c1894fe50
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2023-05-03 00:38:38 +0300

    styling the footer
    also, adding a white version of the logo

--- easybank/src/Component/Footer/Footer.jsx
+++ easybank/src/Component/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import classes from "./Footer.module.scss";
 
 import Button from "../UI/Button";
 
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/logo-white.svg";
 import facebook from "../../assets/icon-facebook.svg";
 import youtube from "../../assets/icon-youtube.svg";
 import twitter from "../../assets/icon-twitter.svg";
@@ -12,61 +12,63 @@ import instagram from "../../assets/icon-instagram.svg";
 export default function Footer() {
   return (
     <footer className={classes.container}>
-      <div>
-        <img src={logo} alt="easy bank logo" />
-        <nav aria-label="Social media">
+      <nav>
+        <div className={classes.social}>
+          <img src={logo} alt="easy bank logo" />
+          <nav aria-label="Social media">
+            <ul>
+              <li>
+                <a href="#">
+                  <img src={facebook} alt="Facebook logo" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={youtube} alt="Youtube logo" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={twitter} alt="Twitter logo" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={pinterest} alt="Pinterest logo" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={instagram} alt="Instagram logo" />
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <nav aria-label="secondary" className={classes.secondary}>
           <ul>
             <li>
-              <a href="#">
-                <img src={facebook} alt="Facebook logo" />
-              </a>
+              <a href="#">About Us</a>
             </li>
             <li>
-              <a href="#">
-                <img src={youtube} alt="Youtube logo" />
-              </a>
+              <a href="#">Contact</a>
             </li>
             <li>
-              <a href="#">
-                <img src={twitter} alt="Twitter logo" />
-              </a>
+              <a href="#">Blog</a>
             </li>
             <li>
-              <a href="#">
-                <img src={pinterest} alt="Pinterest logo" />
-              </a>
+              <a href="#">Careers</a>
             </li>
             <li>
-              <a href="#">
-                <img src={instagram} alt="Instagram logo" />
-              </a>
+              <a href="#">Support</a>
+            </li>
+            <li>
+              <a href="#">Privacy Policy</a>
             </li>
           </ul>
         </nav>
-      </div>
-      <nav aria-label="secondary">
-        <ul>
-          <li>
-            <a href="#">About Us</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-            <a href="#">Blog</a>
-          </li>
-          <li>
-            <a href="#">Careers</a>
-          </li>
-          <li>
-            <a href="#">Support</a>
-          </li>
-          <li>
-            <a href="#">Privacy Policy</a>
-          </li>
-        </ul>
       </nav>
-      <div>
+      <div className={classes.button}>
         <Button />
         <p>© Easybank. All Rights Reserved</p>
       </div>
