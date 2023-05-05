7b9a28db30c6af1e5f473090030e652287e29743
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2023-05-06 00:15:31 +0300

    adding background for the main section

--- easybank/src/Component/Content/MainSection/MainSection.jsx
+++ easybank/src/Component/Content/MainSection/MainSection.jsx
@@ -8,28 +8,30 @@ import backgroundMobile from "../../../assets/bg-intro-mobile.svg";
 
 export default function MainSection() {
   return (
-    <section className={classes.container}>
-      <div className={classes.info}>
-        <h1>Next genetation digital banking</h1>
-        <p>
-          Take your financial life online. Your Easybank account will be a
-          one-stop-shop for spending, saving, budgeting, investing, and much
-          more.
-        </p>
-        <Button></Button>
-      </div>
-      <div className={classes.photos}>
-        <img
-          src={mockups}
-          alt="mockups for the app"
-          className={classes.mockups}
-        />
-        <img
-          src={backgroundMobile}
-          alt="shapes"
-          className={classes.background}
-        />
-      </div>
-    </section>
+    <div className={classes.backgroundColor}>
+      <section className={classes.container}>
+        <div className={classes.info}>
+          <h1>Next genetation digital banking</h1>
+          <p>
+            Take your financial life online. Your Easybank account will be a
+            one-stop-shop for spending, saving, budgeting, investing, and much
+            more.
+          </p>
+          <Button></Button>
+        </div>
+        <div className={classes.photos}>
+          <img
+            src={mockups}
+            alt="mockups for the app"
+            className={classes.mockups}
+          />
+          <img
+            src={backgroundMobile}
+            alt="shapes"
+            className={classes.background}
+          />
+        </div>
+      </section>
+    </div>
   );
 }
